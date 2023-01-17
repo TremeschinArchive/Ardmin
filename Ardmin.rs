a609cf04c160d29d664b3ec5da3cab1cbce34164
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2023-01-17 19:15:38 -0400

    Fix serious bug on accidentally deleting all source files

--- Ardmin.rs
+++ Ardmin.rs
@@ -27,9 +27,9 @@ struct Args {
 
     // Optimizations
     #[arg(short, long, help="· (Optimization) Remove unused Source files (MIDI, WAV)")]
-    sources: bool,
+    unused: bool,
 
-    #[arg(short, long, help="· (Optimization) Remove old plugin states")]
+    #[arg(short, long, help="· (Optimization) Remove old plugin states (5% chance of breaking session??)")]
     states: bool,
 
     #[arg(short, long, help="· (Optimization) Remove backup (.bak) of sessions")]
@@ -70,7 +70,7 @@ fn main() {
                 if (args.backup  || args.all) && (ext == "bak"    ) {Protostar::remove(file.clone())}
 
                 // Search for used MIDI sources
-                if (args.sources || args.all) && (ext == "ardour" ) {
+                if (args.unused  || args.all) && (ext == "ardour" ) {
 
                     // Iterate on .ardour session file lines
                     for line in BufReader::new(File::open(file).unwrap()).lines().map(Result::unwrap) {
@@ -78,7 +78,7 @@ fn main() {
                         // Optimization: Only match lines that contains sources extensions
                         if ! &sourcesExtensions.iter().any(|substring| line.contains(substring)) {continue;}
 
-                        // Match for sources on Ardour session
+                        // // Match for sources on Ardour session
                         for extension in &sourcesExtensions {
                             for capture in Regex::new(format!("name=\"(.*?){}\"", extension).as_str()).unwrap().captures_iter(&line) {
                                 sources.push(format!("{}{}", &capture[1], extension));
@@ -90,27 +90,31 @@ fn main() {
         }
 
         // Recurse on interchange (sources) of session, remove files not listed in sources in any of .ardour sessions
-        for source in Protostar::betterGlob(session.join("interchange").join("**").join("*")) {
-            if source.is_file() && !sources.contains(&source.file_name().unwrap().to_str().unwrap().to_string()) {
-                Protostar::remove(source)
+        if args.unused || args.all {
+            for source in Protostar::betterGlob(session.join("interchange").join("**").join("*")) {
+                if source.is_file() && !sources.contains(&source.file_name().unwrap().to_str().unwrap().to_string()) {
+                    Protostar::remove(source)
+                }
             }
         }
 
-        // Optimization: Remove old plugin states
-        for pluginFolder in Protostar::betterGlob(session.join("plugins").join("*")) {
+        if args.states || args.all {
+            // Optimization: Remove old plugin states
+            for pluginFolder in Protostar::betterGlob(session.join("plugins").join("*")) {
 
-            // Converts session/plugins/stateXYZ to i64 XYZ
-            let getState = |x: &PathBuf| {
-                x.file_name().unwrap().to_str().unwrap().replace("state", "").parse().unwrap()
-            };
+                // Converts session/plugins/stateXYZ to i64 XYZ
+                let getState = |x: &PathBuf| {
+                    x.file_name().unwrap().to_str().unwrap().replace("state", "").parse().unwrap()
+                };
 
-            // The max allowed state
-            let maxState: i64 = Protostar::betterGlob(pluginFolder.join("*")).iter().map(|x| getState(x)).max().unwrap();
+                // The max allowed state
+                let maxState: i64 = Protostar::betterGlob(pluginFolder.join("*")).iter().map(|x| getState(x)).max().unwrap();
 
-            // Delete folders that lag behind the max state
-            for stateFolder in Protostar::betterGlob(pluginFolder.join("*")) {
-                if getState(&stateFolder) < maxState {
-                    Protostar::remove(stateFolder);
+                // Delete folders that lag behind the max state
+                for stateFolder in Protostar::betterGlob(pluginFolder.join("*")) {
+                    if getState(&stateFolder) < maxState {
+                        Protostar::remove(stateFolder);
+                    }
                 }
             }
         }
