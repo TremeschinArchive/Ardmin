4d17d712a7b89cbaab7219654312591a963ab490
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2023-01-17 23:13:38 -0400

    Current Files

---
+++ Ardmin.rs
@@ -0,0 +1,117 @@
+// | (c) Tremeschin, AGPLv3-only License | Ardmin Project | //
+#![allow(non_snake_case)]
+extern crate Protostar;
+use Protostar::*;
+
+// -----------------------------------------------------------------------------------------------|
+
+const ABOUT: &str = "
+Ardmin, an Ardour Session Minimizer.
+
+(c) 2023 Tremeschin, AGPLv3-only License.";
+
+#[derive(Parser, Debug)]
+#[command(author="Tremeschin", version, about=ABOUT)]
+struct Args {
+    #[arg(short, long, help="(Global      ) Path to a Folder of Ardour Sessions")]
+    path: String,
+    #[arg(       long, help="(Global      ) Move existing exports files to other path", default_value_t=str!(""))]
+    exports: String,
+    #[arg(short, long, help="(Global      ) Apply all optimizations")]
+    all: bool,
+    #[arg(short, long, help="(Optimization) Remove unused Source files (MIDI, WAV)")]
+    unused: bool,
+    #[arg(short, long, help="(Optimization) Remove old plugin states (5% chance of breaking per-plugin??)")]
+    states: bool,
+    #[arg(short, long, help="(Optimization) Remove backup (.bak) of sessions")]
+    backup: bool,
+    #[arg(       long, help="(Optimization) Remove history (.history) of sessions")]
+    history: bool,
+    #[arg(short, long, help="(Optimization) Remove analysis, dead, peaks folders")]
+    residuals: bool,
+}
+
+// -----------------------------------------------------------------------------------------------|
+
+fn main() {
+    Protostar::setupLog();
+    let args = Args::parse();
+
+    // Main "for each session"
+    for session in Protostar::betterGlob(PathBuf::from(args.path).join("*")) {
+        if session.is_file() {continue;}
+        info!(":: Optimizing session [{}]", session.display());
+
+        // Optimization: Remove analysis, dead, peaks
+        if args.residuals || args.all {
+            for folder in vec!("analysis", "dead", "peaks") {
+                Protostar::remove(session.join(&folder));
+            }
+        }
+
+        // List of regex to apply searching for sources
+        let mut regrets: HashMap<&str, Regex> = HashMap::new();
+        let mut sources: Vec<String> = vec!();
+
+        for extension in vec!(".mid", ".wav") {
+            let regex = Regex::new(format!("name=\"(.*?){}\"", extension).as_str());
+            regrets.insert(extension, regex.unwrap());
+        }
+
+        // Optimization: Remove .history or .backup or unused MIDI / WAV files
+        for file in Protostar::betterGlob(session.join("*")) {
+            if let Some(ext) = file.extension() {
+                if (args.history || args.all) && (ext == "history") {Protostar::remove(file.clone())}
+                if (args.backup  || args.all) && (ext == "bak"    ) {Protostar::remove(file.clone())}
+
+                // Search for used MIDI sources
+                if (args.unused  || args.all) && (ext == "ardour" ) {
+
+                    // Iterate on .ardour session file lines
+                    for line in BufReader::new(File::open(file).unwrap()).lines().map(Result::unwrap) {
+
+                        // Code optimization: Will not find any more sources
+                        if line.contains("</Sources>") {break}
+
+                        // Match any regex for sources
+                        for (extension, regex) in regrets.iter() {
+                            for capture in regex.captures_iter(&line) {
+                                sources.push(format!("{}{}", &capture[1], extension));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Recurse on interchange (sources) of session, remove files not listed in sources in any of .ardour sessions
+        if args.unused || args.all {
+            for source in Protostar::betterGlob(session.join("interchange").join("**").join("*")) {
+                if source.is_file() && !sources.contains(&source.file_name().unwrap().to_str().unwrap().to_string()) {
+                    Protostar::remove(source)
+                }
+            }
+        }
+
+        if args.states || args.all {
+            // Optimization: Remove old plugin states
+            for pluginFolder in Protostar::betterGlob(session.join("plugins").join("*")) {
+
+                // Converts session/plugins/stateXYZ to i64 XYZ
+                let getState = |x: &PathBuf| {
+                    x.file_name().unwrap().to_str().unwrap().replace("state", "").parse().unwrap()
+                };
+
+                // The max allowed state
+                let maxState: i64 = Protostar::betterGlob(pluginFolder.join("*")).iter().map(|x| getState(x)).max().unwrap();
+
+                // Delete folders that lag behind the max state
+                for stateFolder in Protostar::betterGlob(pluginFolder.join("*")) {
+                    if getState(&stateFolder) < maxState {
+                        Protostar::remove(stateFolder);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,55 @@
+👆【☰】Table of Contents
+
+<div align="center">
+    <h1>Ardmin</h1>
+</div>
+
+Ardour Session Minimizer, a blazing fast small CLI tool to simplify (in size) an Ardour Session Folder by deleting unused sources (WAV, MIDI), old plugin states and _somewhat_ non important files.
+
+<sub><i><b>Note:</b> Backup, backup, backup, backup before using this tool.</i></sub>
+
+# ● Installation
+
+### ▸ Precompiled binary
+Grab the latest binary release [here](https://github.com/BrokenSource/Ardmin/releases/latest)
+
+### ▸ Running from the Source Code
+Head over to the [Protostar Monorepo](https://github.com/Tremeschin/Protostar) for instructions on bootstrapping and running the Projects.
+
+# ● Usage
+
+Tested Ardour versions: `7.2-1`
+
+```
+$ ardmin -h
+Ardmin, an Ardour Session Minimizer.
+
+(c) 2023 Tremeschin, MIT License.
+
+Usage: Ardmin [OPTIONS] --path <PATH>
+
+Options:
+  -p, --path <PATH>        (Global      ) Path to a Folder of Ardour Sessions
+  -a, --all                (Global      ) Apply all optimizations
+  -u, --unused             (Optimization) Remove unused Source files (MIDI, WAV)
+  -s, --states             (Optimization) Remove old plugin states (5% chance of breaking per-plugin??)
+  -b, --backup             (Optimization) Remove backup (.bak) of sessions
+      --history            (Optimization) Remove history (.history) of sessions
+  -r, --residuals          (Optimization) Remove analysis, dead, peaks folders
+  -h, --help               Print help
+  -V, --version            Print version
+```
+
+Apply all optimizations possible:
+- `./ardmin -p ~/Path/To/Ardour/Sessions -a`
+
+Apply only states and unused optimizations:
+- `./ardmin -p ~/Path/To/Ardour/Sessions -s -u`
+
+# ● Warnings
+
+<i><b>Note:</b> Plugin states optimization behaviour seems a bit random, at times it might change a setting or two on the current presets or break entirely. Only happened with Vital and TAL Filter 2 for me, rarely.</i>
+
+# ● License
+
+AGPLv3-only License.
